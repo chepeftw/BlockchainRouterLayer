1b5681c29f463f749fb9baa797de0ba83eafdfe7
Chepe FTW <chepeftw@gmail.com>
2018-05-06 20:56:07 +0200

    Multiple enhancements for the last version.

--- Router.go
+++ Router.go
@@ -5,70 +5,77 @@ import (
 	"net"
 	"time"
 	"strconv"
-    "encoding/json"
+	"encoding/json"
 
-    "github.com/op/go-logging"
-    "github.com/chepeftw/treesiplibs"
+	"github.com/op/go-logging"
 	"github.com/chepeftw/bchainlibs"
 )
-
 
 // +++++++++++++++++++++++++++
 // +++++++++ Go-Logging Conf
 // +++++++++++++++++++++++++++
 var log = logging.MustGetLogger("router")
 
-var me net.IP = net.ParseIP(bchainlibs.LocalhostAddr)
+var me = net.ParseIP(bchainlibs.LocalhostAddr)
 
 // +++++++++ Routing Protocol
-var forwarded map[string]bool = make(map[string]bool)
-var packets map[string]bchainlibs.Packet = make(map[string]bchainlibs.Packet)
+var forwarded = make(map[string]bool)
+var packets = make(map[string]bchainlibs.Packet)
 
 // +++++++++ Channels
 var input = make(chan string)
 var output = make(chan string)
 var blockchain = make(chan string)
 var miner = make(chan string)
+var raft = make(chan string)
 var resending = make(chan int)
 var done = make(chan bool)
 
-
 func sendMessage(payload bchainlibs.Packet) {
-	bchainlibs.SendGeneric( output, payload, log )
-	log.Debug("Sending Packet with TID " + payload.TID + " to channel output")
+	bchainlibs.SendGeneric(output, payload, log)
+	log.Debug("Sending Packet with ID " + payload.ID + " to channel output")
 
 	go func() {
-		time.Sleep( time.Second * time.Duration( 1 ) )
-		log.Debug("RE attendResendingChannel => 1" )
+		time.Sleep(time.Second * time.Duration(1))
+		log.Debug("RE attendResendingChannel => 1")
 		resending <- 1
 	}()
 }
 
+func sendInternal(name string, payload bchainlibs.Packet, channel chan string) {
+	bchainlibs.SendGeneric(channel, payload, log)
+	log.Debug("Sending Packet with ID " + payload.ID + " to channel " + name)
+}
+
 func sendBlockchain(payload bchainlibs.Packet) {
-	bchainlibs.SendGeneric( blockchain, payload, log )
-	log.Debug("Sending Packet with TID " + payload.TID + " to channel blockchain")
+	sendInternal("blockchain", payload, blockchain)
 }
-
 func sendMiner(payload bchainlibs.Packet) {
-	bchainlibs.SendGeneric( miner, payload, log )
-	log.Debug("Sending Packet with TID " + payload.TID + " to channel miner")
+	sendInternal("miner", payload, miner)
 }
-
+func sendRaft(payload bchainlibs.Packet) {
+	sendInternal("raft", payload, raft)
+}
 
 // Function that handles the output channel
 func attendOutputChannel() {
-	log.Debug("Starting output channel")
-	bchainlibs.SendToNetwork( bchainlibs.BroadcastAddr, bchainlibs.RouterPort, output, true, log , me )
+	log.Info("Starting output channel")
+	bchainlibs.SendToNetwork(bchainlibs.BroadcastAddr, bchainlibs.RouterPort, output, true, log, me)
+}
+
+func attendInternalChannel( name string, port string, channel <-chan string ) {
+	log.Info("Starting " + name + " channel")
+	bchainlibs.SendToNetwork(me.String(), port, channel, false, log, me)
 }
 
 func attendBlockchainChannel() {
-	log.Debug("Starting blockchain channel")
-	bchainlibs.SendToNetwork( me.String(), bchainlibs.BlockCPort, blockchain, false, log, me )
+	attendInternalChannel("blockchain", bchainlibs.BlockchainPort, blockchain)
 }
-
 func attendMinerChannel() {
-	log.Debug("Starting miner channel")
-	bchainlibs.SendToNetwork( me.String(), bchainlibs.MinerPort, miner, false, log, me )
+	attendInternalChannel("miner", bchainlibs.MinerPort, miner)
+}
+func attendRaftChannel() {
+	attendInternalChannel("raft", bchainlibs.RaftPort, raft)
 }
 
 func attendResendingChannel() {
@@ -80,15 +87,16 @@ func attendResendingChannel() {
 			for v, k := range forwarded {
 				if !k {
 					missingAck = missingAck + 1
-					log.Debug("Packet " + v + " not acked yet!")
-					sendMessage( packets[v] )
+					log.Info("Packet " + v + " not acked yet!")
+					log.Debug("RE_SEND_MESSAGE=1")
+					sendMessage(packets[v])
 				}
 			}
 
 			if missingAck > 0 {
 				go func() {
-					time.Sleep( time.Second * time.Duration( 1 ) )
-					log.Debug("RE attendResendingChannel => 1" )
+					time.Sleep(time.Second * time.Duration(1))
+					log.Debug("RE attendResendingChannel => 1")
 					resending <- 1
 				}()
 			}
@@ -99,203 +107,237 @@ func attendResendingChannel() {
 // Function that handles the buffer channel
 func attendInputChannel() {
 	log.Debug("Starting input channel")
-    for {
-	j, more := <-input
-	if more {
-	    // First we take the json, unmarshal it to an object
-	    payload := bchainlibs.Packet{}
-	    json.Unmarshal([]byte(j), &payload)
+	for {
+		j, more := <-input
+		if more {
+			// First we take the json, unmarshal it to an object
+			payload := bchainlibs.Packet{}
+			json.Unmarshal([]byte(j), &payload)
 
-		//log.Debug("---------------------------")
-		//log.Debug("Something arrived")
-		//log.Debug(j)
+			//log.Debug("---------------------------")
+			//log.Debug("Something arrived")
+			//log.Debug(j)
 
-		source := payload.Source
-		tid := payload.TID
+			source := payload.Source
+			id := payload.ID
 
-		switch payload.Type {
+			switch payload.Type {
 
-		case bchainlibs.InternalUBlockType:
-			log.Debug("Receiving InternalUBlockType Packet")
-			//if eqIp( me, source ) {
-			payload.Type = bchainlibs.UBlockType
-			forwarded["u"+tid] = false
-			packets["u"+tid] = payload
-			sendMessage( payload )
-			log.Info("U_BLOCK_TIME_RECEIVED=" + strconv.FormatInt(time.Now().UnixNano(), 10) + "," + tid)
-			//}
-		break
+			case bchainlibs.InternalPong:
+				log.Info("Receving PONG = " + id)
+				break
 
-		case bchainlibs.InternalVBlockType:
-			log.Debug("Receiving InternalVBlockType Packet")
-			//if eqIp( me, source ) {
-			payload.Type = bchainlibs.VBlockType
-			forwarded["v"+tid] = false
-			packets["v"+tid] = payload
-			sendBlockchain( payload )
-			sendMessage( payload )
-			log.Info("V_BLOCK_TIME_RECEIVED=" + strconv.FormatInt(time.Now().UnixNano(), 10) + "," + tid)
-			//}
-		break
+			case bchainlibs.InternalQueryType:
+				if _, ok := forwarded[ "q"+id ]; !ok {
+					log.Info("Receiving InternalQueryType Packet")
+					// This is the start of the query, which later can be queried in MongoDB as the minimal value of this.
+					// Then compared to the maximun I can get the time it took to propagate.
+					// This is fine cause there is just one query
+					log.Debug("QUERY_TIME_RECEIVED=" + strconv.FormatInt(time.Now().UnixNano(), 10))
+					payload.Type = bchainlibs.QueryType
+					forwarded[ "q"+id ] = false
+					packets["q"+id] = payload
 
-		case bchainlibs.UBlockType:
-			if _, ok := forwarded[ "u"+tid ]; !ok && !eqIp( me, source ) {
-				log.Debug("Receiving UBlockType Packet")
-				forwarded[ "u"+tid ] = true
-				sendMiner( payload )
-				sendMessage( payload )
-				log.Info("U_BLOCK_TIME_RECEIVED=" + strconv.FormatInt(time.Now().UnixNano(), 10) + "," + tid)
-			} else if !forwarded[ "u"+tid ] {
-				forwarded[ "u"+tid ] = true
+					sendBlockchain(payload)
+					sendMessage(payload)
+				}
+				break
+
+			// ----------------------------
+			// From X
+			// ----------------------------
+
+			case bchainlibs.QueryType:
+				if _, ok := forwarded[ "q"+id ]; !ok && !eqIp(me, source) {
+					log.Info("Receiving QueryType Packet")
+					log.Debug("QUERY_TIME_RECEIVED=" + strconv.FormatInt(time.Now().UnixNano(), 10))
+					forwarded[ "q"+id ] = true
+
+					sendBlockchain(payload)
+					sendMessage(payload)
+				} else if !forwarded[ "q"+id ] {
+					forwarded[ "q"+id ] = true
+				}
+				break
+
+			case bchainlibs.TransactionType:
+				if _, ok := forwarded[ "t"+id ]; !ok && !eqIp(me, source) {
+					log.Info("Receiving TransactionType Packet")
+					forwarded[ "t"+id ] = true
+					sendBlockchain(payload)
+					sendMessage(payload)
+				} else if !forwarded[ "t"+id ] {
+					forwarded[ "t"+id ] = true
+				}
+				break
+
+			case bchainlibs.BlockType:
+				if _, ok := forwarded[ "b"+id ]; !ok && !eqIp(me, source) {
+					log.Info("Receiving BlockType Packet")
+					forwarded[ "b"+id ] = true
+					sendBlockchain(payload)
+					sendMessage(payload)
+				} else if !forwarded[ "b"+id ] {
+					forwarded[ "b"+id ] = true
+				}
+				break
+
+
+			// ----------------------------
+			// From Blockchain
+			// ----------------------------
+
+			case bchainlibs.LastBlockType:
+				log.Info("Receiving LastBlockType Packet")
+				sendMiner(payload)
+				break
+
+			case bchainlibs.LaunchElection:
+				log.Info("Receiving LaunchElection Packet")
+
+				start := bchainlibs.CreateRaftStartPacket(me)
+				sendRaft(start)
+
+				sendMiner(payload)
+				break
+
+			case bchainlibs.InternalTransactionType:
+				log.Info("Receiving InternalTransactionType Packet")
+
+				payload.Type = bchainlibs.TransactionType
+				forwarded["t"+id] = false
+				packets["t"+id] = payload
+
+				sendMessage(payload)
+				//log.Debug("TRANSACTION_TIME_RECEIVED=" + strconv.FormatInt(time.Now().UnixNano(), 10) + "," + id)
+				break
+
+
+			// ----------------------------
+			// From Miner
+			// ----------------------------
+
+			case bchainlibs.InternalBlockType:
+				log.Info("Receiving InternalBlockType Packet")
+
+				payload.Type = bchainlibs.BlockType
+				forwarded["b"+id] = false
+				packets["b"+id] = payload
+
+				sendBlockchain(payload)
+				sendMessage(payload)
+				//log.Debug("BLOCK_TIME_RECEIVED=" + strconv.FormatInt(time.Now().UnixNano(), 10) + "," + id)
+				break
+
+
+			// ----------------------------
+			// From Raft
+			// ----------------------------
+
+			case bchainlibs.RaftResult:
+				log.Info("Receiving ResultFromElection Packet")
+				sendMiner(payload)
+
+				stop := bchainlibs.CreateRaftStopPacket(me)
+				sendRaft(stop)
+				break
+
 			}
-		break
 
-		case bchainlibs.VBlockType:
-			if _, ok := forwarded[ "v"+tid ]; !ok && !eqIp( me, source ) {
-				log.Debug("Receiving VBlockType Packet")
-				forwarded[ "v"+tid ] = true
-				sendBlockchain( payload )
-				sendMessage( payload )
-				log.Info("V_BLOCK_TIME_RECEIVED=" + strconv.FormatInt(time.Now().UnixNano(), 10) + "," + tid)
-			} else if !forwarded[ "v"+tid ] {
-				forwarded[ "v"+tid ] = true
-			}
-		break
-
-
-
-		case bchainlibs.LastBlockType:
-			//if eqIp( me, source ) {
-			log.Debug("Receiving LastBlockType Packet")
-			sendMiner( payload )
-			//}
-		break
-
-		case bchainlibs.InternalQueryType:
-			if _, ok := forwarded[ "q"+tid ]; !ok{
-				log.Debug("Receiving InternalQueryType Packet")
-				// This is the start of the query, which later can be queried in MongoDB as the minimal value of this.
-				// Then compared to the maximun I can get the time it took to propagate.
-				// This is fine cause there is just one query
-				log.Info("QUERY_TIME_RECEIVED=" + strconv.FormatInt(time.Now().UnixNano(), 10))
-				payload.Type = bchainlibs.QueryType
-				forwarded[ "q"+tid ] = false
-				packets["q"+tid] = payload
-				sendBlockchain( payload )
-				sendMessage( payload )
-			}
-		break
-
-		case bchainlibs.QueryType:
-			if _, ok := forwarded[ "q"+tid ]; !ok && !eqIp( me, source ) {
-				log.Debug("Receiving QueryType Packet")
-				log.Info("QUERY_TIME_RECEIVED=" + strconv.FormatInt(time.Now().UnixNano(), 10))
-				forwarded[ "q"+tid ] = true
-				sendBlockchain( payload )
-				sendMessage( payload )
-			} else if !forwarded[ "q"+tid ] {
-				forwarded[ "q"+tid ] = true
-			}
-		break
-
-		case bchainlibs.InternalPong:
-			log.Info("Receving PONG = " + tid)
-		break
-
+		} else {
+			log.Debug("closing channel")
+			done <- true
+			return
 		}
 
-	} else {
-	    log.Debug("closing channel")
-	    done <- true
-	    return
 	}
-
-    }
 }
 
-func eqIp( a net.IP, b net.IP ) bool {
-    return treesiplibs.CompareIPs(a, b)
+func eqIp(a net.IP, b net.IP) bool {
+	return bchainlibs.CompareIPs(a, b)
 }
 
 func pingInternals() {
 	time.Sleep(time.Second * time.Duration(2))
 
-	payload := bchainlibs.AssemblePing( me )
-	sendBlockchain( payload )
+	payload := bchainlibs.BuildPing(me)
+	sendBlockchain(payload)
 
 	time.Sleep(time.Second * time.Duration(1))
 
-	payload = bchainlibs.AssemblePing( me )
-	sendMiner( payload )
+	payload = bchainlibs.BuildPing(me)
+	sendMiner(payload)
 }
-
 
 func main() {
 
-    confPath := "/app/conf.yml"
-    if len(os.Args[1:]) >= 1 {
+	confPath := "/app/conf.yml"
+	if len(os.Args[1:]) >= 1 {
 		confPath = os.Args[1]
-    }
-    var c bchainlibs.Conf
-    c.GetConf( confPath )
+	}
+	var c bchainlibs.Conf
+	c.GetConf(confPath)
 
-    targetSync := c.TargetSync
+	targetSync := c.TargetSync
 	logPath := c.LogPath
 
-    // Logger configuration
-	f := bchainlibs.PrepareLog( logPath, "router" )
+	// Logger configuration
+	f := bchainlibs.PrepareLog(logPath, "router")
 	defer f.Close()
 	backend := logging.NewLogBackend(f, "", 0)
 	backendFormatter := logging.NewBackendFormatter(backend, bchainlibs.LogFormat)
 	backendLeveled := logging.AddModuleLevel(backendFormatter)
 	backendLeveled.SetLevel(logging.DEBUG, "")
-	logging.SetBackend( backendLeveled )
+	logging.SetBackend(backendLeveled)
 
-    log.Info("")
-    log.Info("------------------------------------------------------------------------")
-    log.Info("")
-    log.Info("Starting Routing process, waiting some time to get my own IP...")
+	log.Info("")
+	log.Info("------------------------------------------------------------------------")
+	log.Info("")
+	log.Info("Starting Routing process, waiting some time to get my own IP...")
 
 	// Wait for sync
-	bchainlibs.WaitForSync( targetSync, log )
+	bchainlibs.WaitForSync(targetSync, log)
 
-    // But first let me take a selfie, in a Go lang program is getting my own IP
-    me = treesiplibs.SelfieIP()
-    log.Info("Good to go, my ip is " + me.String())
+	// But first let me take a selfie, in a Go lang program is getting my own IP
+	me = bchainlibs.SelfieIP()
+	log.Info("Good to go, my ip is " + me.String())
 
-    // Lets prepare a address at any address at port bchainlibs.RouterPort
-    ServerAddr,err := net.ResolveUDPAddr(bchainlibs.Protocol, bchainlibs.RouterPort)
-    treesiplibs.CheckError(err, log)
+	// Lets prepare a address at any address at port bchainlibs.RouterPort
+	ServerAddr, err := net.ResolveUDPAddr(bchainlibs.Protocol, bchainlibs.RouterPort)
+	bchainlibs.CheckError(err, log)
 
-    // Now listen at selected port
-    ServerConn, err := net.ListenUDP(bchainlibs.Protocol, ServerAddr)
-    treesiplibs.CheckError(err, log)
-    defer ServerConn.Close()
+	// Now listen at selected port
+	ServerConn, err := net.ListenUDP(bchainlibs.Protocol, ServerAddr)
+	bchainlibs.CheckError(err, log)
+	defer ServerConn.Close()
 
-    // Run the Input!
-    go attendInputChannel()
-    // Run the Output! The channel for communicating with the outside world!
+	// Run the Input!
+	go attendInputChannel()
+
+	// Run the Output! The channel for communicating with the outside world!
 	// The broadcast to all the MANET
-    go attendOutputChannel()
+	go attendOutputChannel()
+
 	// Run the Internal channel! The direct messages to the app layer
 	go attendBlockchainChannel()
 	go attendMinerChannel()
+	go attendRaftChannel()
 
 	go attendResendingChannel()
 
 	go pingInternals()
 
-    buf := make([]byte, 1024)
+	buf := make([]byte, 1024)
 
-    for {
-		n,_,err := ServerConn.ReadFromUDP(buf)
+	for {
+		n, _, err := ServerConn.ReadFromUDP(buf)
 		input <- string(buf[0:n])
-		treesiplibs.CheckError(err, log)
-    }
+		bchainlibs.CheckError(err, log)
+	}
 
-    close(input)
-    close(output)
-    close(blockchain)
+	close(input)
+	close(output)
+	close(blockchain)
 
-    <-done
+	<-done
 }
